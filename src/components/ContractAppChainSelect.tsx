134954c101479df9e5def869bf9f7c3b99ca2312
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-04-30 18:26:53 +0100

    added chain selector

---
+++ src/components/ContractAppChainSelect.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+
+import IssuerAnimation from "./IssuingAnimation";
+import { HARMONYONEISSUERLINK, HARMONYONEDAOLINK } from "../links";
+const daoButtonStyle = {
+  margin: "0 auto",
+  marginTop: "10px",
+  display: "flex",
+  flexDirection: "column",
+};
+
+export interface SimpleDialogProps {
+  open: boolean;
+  dao: boolean;
+  onClose: () => void;
+}
+
+export interface ContractAppChainSelectProps {
+  dao: boolean;
+}
+
+enum ListItemChain {
+  Harmony,
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const { onClose, open } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleListItemClick = (value: ListItemChain) => {
+    onClose();
+    if (value === ListItemChain.Harmony) {
+      if (props.dao) {
+        window.location.href = HARMONYONEDAOLINK;
+      } else {
+        window.location.href = HARMONYONEISSUERLINK;
+      }
+    }
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Select Chain</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        <ListItem
+          button
+          onClick={() => handleListItemClick(ListItemChain.Harmony)}
+        >
+          <ListItemAvatar>
+            <img src="/harmonyOneLogo.svg" width={"30px"} />
+          </ListItemAvatar>
+          <ListItemText primary={"Harmony One"} />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+}
+
+export default function ContractAppChainSelect(
+  props: ContractAppChainSelectProps
+) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  if (props.dao) {
+    return (
+      <div>
+        <Button
+          variant="contained"
+          style={daoButtonStyle}
+          onClick={handleClickOpen}
+        >
+          <img src="/DAO.png" width="100px" />
+          <small>Click to enter the DAO</small>
+        </Button>
+        <SimpleDialog dao={props.dao} open={open} onClose={handleClose} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+          <IssuerAnimation></IssuerAnimation>
+        </Button>
+        <SimpleDialog dao={props.dao} open={open} onClose={handleClose} />
+      </div>
+    );
+  }
+}
