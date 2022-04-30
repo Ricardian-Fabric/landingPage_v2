87e8fbe95c31d7c68c19c15ad221dfd2f7078282
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-04-30 18:26:43 +0100

    application links added

---
+++ src/links.ts
@@ -0,0 +1,4 @@
+export const HARMONYONEISSUERLINK =
+  "https://ervded4meifzaaglk6j5sxqmioenu4ibajhx4o3m7x2stsbi.arweave.net/JGoyD4wiC5AAy1eT-2V4MQ4jacQECT347bP-31Kcgo8";
+export const HARMONYONEDAOLINK =
+  "https://ervded4meifzaaglk6j5sxqmioenu4ibajhx4o3m7x2stsbi.arweave.net/JGoyD4wiC5AAy1eT-2V4MQ4jacQECT347bP-31Kcgo8";
