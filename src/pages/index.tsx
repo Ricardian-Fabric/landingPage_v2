ba11096064046e561fadc91fe9787a57d17d033c
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-05-26 17:18:52 +0100

    updated landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Button, Divider, Fade, Paper, Typography, Stack } from "@mui/material";
-import Possibilities from "../components/Possibilities";
+import { Button, Paper, Typography, Stack } from "@mui/material";
 import Spacer from "../components/Spacer";
-import { Roadmap } from "../components/Roadmap";
 import ContractAppChainSelect from "../components/ContractAppChainSelect";
 import { Helmet } from "react-helmet";
+import { NokodeSketch } from "../components/nokodeSketch";
+import { HowItWorks, IssueLegalContract } from "../components/howItWorks";
 const lightTheme = createTheme({
   palette: {
     mode: "light",
@@ -30,7 +30,7 @@ const lightTheme = createTheme({
   },
 });
 
-const PageTitleShadow = {
+export const PageTitleShadow = {
   textShadow: "2px 2px #999999",
   fontSize: "2em",
 };
@@ -39,7 +39,6 @@ const unstoppobleTypography = {
   marginTop: "20px",
   marginBottom: "20px",
   textShadow: "2px 2px #999999",
-  color: "red",
 };
 
 const row = {
@@ -54,26 +53,45 @@ const imageBorder = {
   boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
 };
 
-const PaperWrap = (props: { children: React.ReactNode }) => {
+const marginRight2px = {
+  marginRight: "10px",
+};
+
+const buyLogo = {
+  fontSize: "20px",
+  marginRight: "10px",
+};
+
+const marginLeftAndRight2px = {
+  marginLeft: "2px",
+  marginRight: "2px",
+};
+
+const daoimage = {
+  maxWidth: "500px",
+};
+
+const PaperWrap = (props: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
     <Paper
+      className={props.className}
       elevation={0}
       sx={{
         paddingBottom: 10,
         paddingLeft: 10,
         paddingRight: 10,
-        borderRadius: "25px",
         textAlign: "center",
         width: "100%",
-        maxWidth: "800px",
         minWidth: "300px",
         alignSelf: "center",
       }}
     >
-      <Divider></Divider>
       <Spacer></Spacer>
       {props.children}
-      <Spacer></Spacer> <Divider></Divider>
+      <Spacer></Spacer>
     </Paper>
   );
 };
@@ -92,213 +110,184 @@ const IndexPage = () => {
           href="https://arweave.net/cGU3w4SlEESxajw-t_mhoBArT7WTwmac1mvjlBqNIi0"
         />
       </Helmet>
-      <MyAppBar></MyAppBar>
-      <Stack spacing={2}>
-        <PaperWrap>
-          <Fade in={true} timeout={4000}>
-            <img
-              width="100%"
-              src="/IssueALegalContractWithASmartContract.png"
-              alt="Issue a Legal Contract with a Smart Contract!"
-            />
-          </Fade>
-          <ContractAppChainSelect dao={false}></ContractAppChainSelect>
+      <div>
+        <MyAppBar></MyAppBar>
+        <Stack spacing={2}>
+          <PaperWrap>
+            <ContractAppChainSelect dao={false}></ContractAppChainSelect>
 
-          <Fade in={true} timeout={4000}>
-            <Typography variant="caption" display="block" gutterBottom>
-              Click on the image to enter the DApp!
-            </Typography>
-          </Fade>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            A Framework for creating Legal P2P Services with Smart Contracts
-          </Typography>
-          <Possibilities></Possibilities>
-          <Spacer></Spacer>
-          <Fade in={true} timeout={4000}>
-            <Button variant="contained" href="https://docs.ricardianfabric.com">
-              How it works?
+            <IssueLegalContract />
+
+            <Typography
+              variant="h3"
+              component="h3"
+              style={PageTitleShadow}
+            ></Typography>
+            <Spacer></Spacer>
+            <NokodeSketch />
+            <HowItWorks />
+          </PaperWrap>
+          {/* <PaperWrap></PaperWrap> */}
+          <PaperWrap>
+            <Spacer></Spacer>
+
+            <Button href="https://global.transak.com/" variant="contained">
+              <h5 style={buyLogo}>Buy</h5>
+              <img
+                src="./harmonyOneLogo.svg"
+                style={marginRight2px}
+                width="20px"
+              />
+              Harmony One and{" "}
+              <img
+                style={marginLeftAndRight2px}
+                src="arweaveLogoIcon.svg"
+                width="20px"
+              />{" "}
+              Arweave here
             </Button>
-          </Fade>
-        </PaperWrap>
-        <PaperWrap>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            An Agreement Protocol for the New Open Web
-          </Typography>
-          <Spacer></Spacer>
-          <img
-            src="/on-chain-agreements.png"
-            width="300px"
-            alt="On-Chain Agreements"
-          />
-          <Spacer></Spacer>
-          <img src="/signerBG.jpg" width="300px" style={imageBorder}></img>
-          <Spacer></Spacer>
-          <Button
-            variant="contained"
-            href="https://docs.ricardianfabric.com/guides/ricardian-contracts"
-          >
-            Sign agreements on-chain using Ricardian Contracts!
-          </Button>
-        </PaperWrap>
 
-        <PaperWrap>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            Business as usual.
-          </Typography>
-          <Spacer></Spacer>
-          <img
-            src="/BusinessAsUsual.jpg"
-            width="300px"
-            style={imageBorder}
-            alt="Business as Usual."
-          />
-          <Spacer></Spacer>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            Adopt innovation.
-          </Typography>
-          <Spacer></Spacer>
-          <img
-            src="/RunYourCompanyOnSmartContracts.png"
-            width="300px"
-            alt="Run Your Company On Smart Contracts"
-          />
-        </PaperWrap>
+            <Spacer></Spacer>
+          </PaperWrap>
 
-        <PaperWrap>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            Permanent Document Storage
-          </Typography>
-          <Spacer></Spacer>
-          <img
-            width="300px"
-            src="/PoweredByThePermaweb.png"
-            alt="Powered by the permaweb"
-          />
-          <Spacer></Spacer>
-          <Typography variant="body1" component="div">
-            Leverage the Power of the Permaweb. Pay once and store your
-            documents forever using the Arweave Blockweave.
-          </Typography>
-          <Button
-            variant="contained"
-            href="https://docs.ricardianfabric.com/guides/permaweb"
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
+          <PaperWrap>
+            <Typography variant="h3" component="h3" style={PageTitleShadow}>
+              Permanent Document Storage
+            </Typography>
+            <Spacer></Spacer>
+
+            <Spacer></Spacer>
+            <Typography variant="body1" component="div">
+              Use Ar and store your data forever using the Arweave Blockweave.
+            </Typography>
+            <Button
+              variant="contained"
+              href="https://docs.ricardianfabric.com/guides/permaweb"
             >
               <img
-                style={{ margin: "0 auto" }}
-                src="/arweaveLogoIcon.svg"
-                alt="a"
-                width="50px"
+                width="300px"
+                src="/PoweredByThePermaweb.png"
+                alt="Powered by the permaweb"
               />
-              Burner Wallet Docs
-            </div>
-          </Button>
-        </PaperWrap>
-        <PaperWrap>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            Leave a Transparent Audit Trail
-          </Typography>
-          <Spacer></Spacer>
-          <img src="/TransparentBubble.jpg" width="300px" style={imageBorder} />
-          <Spacer></Spacer>
-          <Typography variant="body1" component="div">
-            Create Trails to Organize your Permaweb Uploads!
-          </Typography>
-          <Button
-            variant="contained"
-            href="https://docs.ricardianfabric.com/guides/trails"
-          >
-            Find out more
-          </Button>
-        </PaperWrap>
-        <PaperWrap>
-          <img
-            src="/DecentralizedAutonomousOrganization.png"
-            width="100%"
-            alt="Decentralized Autonomous Organization"
-          />
-          <Typography variant="h3" component="h3" style={unstoppobleTypography}>
-            Unstoppable and built on blockchains.
-          </Typography>
-
-          <div style={row}>
+            </Button>
+          </PaperWrap>
+          <PaperWrap>
             <img
-              src="/harmony-one-logo.png"
-              width="150px"
-              alt="Harmony One logo"
+              src="/DecentralizedAutonomousOrganization.png"
+              width="100%"
+              alt="Decentralized Autonomous Organization"
+              style={daoimage}
             />
-            <img src="/arweave-logo.svg" width="150px" alt="Arweave logo" />
-          </div>
-          <Spacer />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <ContractAppChainSelect dao={true}></ContractAppChainSelect>
-          </div>
-        </PaperWrap>
+            <Typography variant="p" component="p" style={unstoppobleTypography}>
+              Permanently available and built on blockchains.
+            </Typography>
 
-        <PaperWrap>
-          <Typography variant="h3" component="h3" style={PageTitleShadow}>
-            The Builder Guild allows anybody to contribute to the application!
-          </Typography>
-
-          <Spacer></Spacer>
-          <img
-            src="/codeHead.png"
-            height="300px"
-            style={imageBorder}
-            alt="Earn to develop"
-          />
-          <Spacer></Spacer>
-          <Button
-            variant="contained"
-            href="https://docs.ricardianfabric.com/guides/dao"
-          >
+            <div style={{ ...row, maxWidth: "500px", margin: "0 auto" }}>
+              <img
+                src="/harmony-one-logo.png"
+                width="150px"
+                alt="Harmony One logo"
+              />
+              <img src="/arweave-logo.svg" width="150px" alt="Arweave logo" />
+            </div>
+            <Spacer />
             <div
               style={{
                 display: "flex",
-                flexDirection: "column",
+                flexDirection: "row",
                 justifyContent: "center",
               }}
             >
-              <img src="/BuilderGuildAndDao.png" width="300px" />
-              Docs
+              <ContractAppChainSelect dao={true}></ContractAppChainSelect>
             </div>
-          </Button>
-          <Spacer></Spacer>
-        </PaperWrap>
-        <PaperWrap>
-          <img src="/Roadmap.png" width="100px" alt="Roadmap" />
-          <Spacer></Spacer>
-          <Roadmap></Roadmap>
-        </PaperWrap>
-        <PaperWrap>
-          <img src="/FollowUs.png" width="100px" alt="Follow us" />
-          <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
-            <Button href="https://discord.gg/Z7eeksAzdh">
-              <img src="/Discord-Logo-Black.png" width="50px" />
-            </Button>
-            <Button href="https://twitter.com/RicardianFabric?ref_src=twsrc%5Etfw">
-              <img src="/2021_Twitter_logo-blue.png" width="50px" />
-            </Button>
-          </Stack>
+          </PaperWrap>
 
-          <Typography variant="subtitle2" component="div">
-            Feel free to reach out to us at admin@ricardianfabric.com
-          </Typography>
-        </PaperWrap>
-        <Spacer />
-      </Stack>
+          <PaperWrap>
+            <Typography variant="h3" component="h3" style={PageTitleShadow}>
+              Develop to earn tokens.
+            </Typography>
+            <Typography variant="h6" component="h6">
+              The Builder Guild allows anybody to contribute to the application!
+            </Typography>
+
+            <Spacer></Spacer>
+            <img
+              src="/codeHead.png"
+              height="300px"
+              style={imageBorder}
+              alt="Earn to develop"
+            />
+            <Spacer></Spacer>
+            <Button
+              variant="contained"
+              href="https://docs.ricardianfabric.com/guides/dao"
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <img src="/BuilderGuildAndDao.png" width="300px" />
+                Docs
+              </div>
+            </Button>
+            <Spacer></Spacer>
+          </PaperWrap>
+          <PaperWrap>
+            <img src="/ComingSoon.png" width="200px" alt="Coming Soon" />
+            <Spacer></Spacer>
+            <Typography variant="h3" component="h3">
+              Escrow Service, NFT with Rights, zkKYC
+            </Typography>
+          </PaperWrap>
+          <PaperWrap>
+            <Typography variant="h3" component="h3" style={PageTitleShadow}>
+              Follow
+            </Typography>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ justifyContent: "center" }}
+            >
+              <Button href="https://discord.gg/Z7eeksAzdh">
+                <img src="/Discord-Logo-Black.png" width="50px" />
+              </Button>
+              <Button href="https://twitter.com/RicardianFabric?ref_src=twsrc%5Etfw">
+                <img src="/2021_Twitter_logo-blue.png" width="50px" />
+              </Button>
+            </Stack>
+
+            <Typography variant="subtitle2" component="div">
+              Feel free to reach out: admin@ricardianfabric.com
+            </Typography>
+            <Spacer></Spacer>
+            <Stack
+              direction="column"
+              spacing={1}
+              sx={{
+                justifyContent: "center",
+                border: "1px dashed grey",
+                maxWidth: "200px",
+                margin: "0 auto",
+              }}
+            >
+              <Button href="https://github.com/Ricardian-Fabric/Ricardian-Fabric/discussions">
+                <img src="/discussions.png" width="100px" />
+              </Button>
+              <Typography
+                variant="subtitle2"
+                component="div"
+                sx={{ color: "red" }}
+              >
+                DAO proposal discussions
+              </Typography>
+            </Stack>
+          </PaperWrap>
+          <Spacer />
+        </Stack>
+      </div>
     </ThemeProvider>
   );
 };
