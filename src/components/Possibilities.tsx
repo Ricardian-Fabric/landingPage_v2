7fe4b12cab2c744c5d492410bb32d1b5f2d770e7
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-05-26 17:19:02 +0100

    updated

--- src/components/Possibilities.tsx
+++ src/components/Possibilities.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import Divider from "@mui/material/Divider";
-import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
-
-const Item = styled(Paper)(({ theme }) => ({
+import Box from "@mui/material/Box";
+const Item = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  paddingRight: "5px",
+  paddingLeft: "5px",
   textAlign: "center",
   color: theme.palette.text.secondary,
   "&:hover": {
@@ -20,7 +21,6 @@ export default function Possibilities() {
     <div style={{ margin: "0 auto", width: "100%" }}>
       <Stack
         direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
         spacing={1}
         sx={{
           marginTop: "20px",
@@ -29,13 +29,13 @@ export default function Possibilities() {
         }}
       >
         <Item>
-          <h4>Intellectual Property Rights</h4>
+          <h4>Rights</h4>
         </Item>
         <Item>
-          <h4>Legally Binding Smart Contracts</h4>
+          <h4>Indemnification</h4>
         </Item>
         <Item>
-          <h4>Regulated P2P Services</h4>
+          <h4>Agreements</h4>
         </Item>
       </Stack>
     </div>
